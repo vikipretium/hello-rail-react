0cbdebb3031f7255f50ad6025513beeaf3468804
vikipretium <vighneshpasupathy@gmail.com>
2022-07-30 19:12:53 +0530

    fix linters

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,7 @@
 class Api::MessagesController < ApplicationController
-	def index
-		random_message = Message.all.sample
-		greeting = random_message.message
-		render json: {greeting: greeting}.to_json
-	end	
+  def index
+    random_message = Message.all.sample
+    greeting = random_message.message
+    render json: { greeting: }.to_json
+  end
 end
